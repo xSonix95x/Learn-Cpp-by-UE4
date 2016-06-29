424ec8f4848875d048976efefd24fccd246524aa
xSonix95x <sonic2009xzc@mail.ru>
2016-06-29 18:37:59 +0300

    #3 add Co004  and Co005

--- Source.cpp
+++ Source.cpp
@@ -3,6 +3,9 @@
 //-----------------------------------//
 //001 - Ask name & age. Enter name & age. Print all
 //002 - Write an isPositive function that returns true when the double parameter passed to it is indeed positive
+//003 - function returns true when the magnitude of 'a' is equal to the magnitude of 'b' (absolute value)
+//004 - Write a getGrade() function that accepts an integer value (marks out of 100) and returns the grade(either A, B, C, D, or F).
+//005 - A mathematical function is of the form f(x) = 3x + 4. Write a C++ function that returns values for f(x).
 //-----------------------------------//
 
 
@@ -31,7 +34,7 @@ int main() {
 }
 
 
-///////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+/////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 
 //Co002 - Write an isPositive function that returns true when the double parameter passed to it is indeed positive
@@ -53,7 +56,7 @@ int main() {
 }
 
 
-///////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+/////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 
 //Co003 - function returns true when the magnitude of 'a' is equal to the magnitude of 'b' (absolute value)
@@ -77,6 +80,67 @@ int main() {
 	cin >> b;
 	
 	cout << a << " and " << b << boolalpha << " is equal ? \nAnswer : " << isEqual(a,b) << endl;
+	system("pause");
+	return 0;
+}
+
+
+/////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+
+//Co004 - Write a getGrade() function that accepts an integer value (marks out of 100) and returns the grade(either A, B, C, D, or F).
+string getGrade(int a)
+{
+	string grade = "";
+	
+	if (a < 20)
+		grade += "F";
+	else if (a < 40)
+		grade += "D";
+	else if (a < 60)
+		grade += "C";
+	else if (a < 80)
+		grade += "B";
+	else grade += "A";
+
+	return grade;
+}
+
+int main() {
+
+	int a;
+
+	cout << "Enter number" << endl;
+	cin >> a;
+
+	if (a < 0)
+		cout << "Less then 0" << endl;
+	else if ( a > 100)
+		cout << "More then 100" << endl;
+	else
+		cout << "You get the grade - " << getGrade(a) << endl;
+	system("pause");
+	return 0;
+}
+
+
+/////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+
+//Co005 - A mathematical function is of the form f(x) = 3x + 4. Write a C++ function that returns values for f(x).
+int getAnswer(int x)
+{
+	return 3 * x + 4;
+}
+
+int main() {
+
+	int x;
+
+	cout << "Enter number" << endl;
+	cin >> x;
+
+	cout << "f(x) = 3 * x + 4 = " << getAnswer(x) << endl;
 	system("pause");
 	return 0;
 }
